733ab1018398f134f926d83bc6fb5b5d0fe7fe70
0x03bb <alibouakaz91@gmail.com>
2024-01-20 19:23:12 +0100

    append , insert & display function

--- linkedlist.c
+++ linkedlist.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 // typedef struct noude ;
 // typedef noude *ndp;
 
@@ -20,16 +21,43 @@ typedef struct noude {
 
 
 ndp insrt(ndp head,int dt){ 
-    ndp hp;       //helper pionter
-    hp = malloc(sizeof(noude));
-    (*hp).data = dt;     //its the same of hp->data
-    (*hp).next = head;
-    return hp;
+    ndp hp , scnr = head;       //helper pionter
+    
+    // if (index == 0){
+        hp = malloc(sizeof(noude));
+        (*hp).data = dt;     //its the same of hp->data
+        (*hp).next = head;
+        return hp;
+    // }
+
+    // else{ 
+    //     for (int cnt = 0; cnt <= index; cnt ++){
+    //       scnr = (*scnr).next; 
+    //     }
+    // }
+    // hp = malloc(sizeof(noude));
+
+}
+void append(ndp head,int dt){ 
+    ndp scnr = head ,new_noude;       //helper pionter
+    
+    new_noude = malloc(sizeof(noude));
+    (*new_noude).data = dt;
+    (*new_noude).next = NULL;
+
+    while ((*scnr).next != NULL){
+        scnr = (*scnr).next;
+        printf("%d\n",scnr);
+    }
+    
+
+    (*scnr).next = new_noude;
 }
 
 void display(ndp head){
     ndp scaner = head;
-
+    printf("start\n");
+    
     while (scaner != NULL){
         printf("%d\n",(*scaner).data);
         scaner = (*scaner).next;
@@ -42,8 +70,8 @@ void main(int argc, char const *argv[]){
     hd = insrt(hd ,1);
     hd = insrt(hd ,2);
     hd = insrt(hd ,3);
-
-    // hd = insrt(hd ,4);
+    insrt_tail(hd,5);
     display(hd);
+    
     
 }
