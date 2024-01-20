11455de95b22f42a091a701950685b8394a7dde8
0x03bb <alibouakaz91@gmail.com>
2024-01-20 22:34:14 +0100

    add insert |Fix: index 0

--- linkedlist.c
+++ linkedlist.c
@@ -2,13 +2,7 @@
 #include <stdio.h>
 
 
-// typedef struct noude ;
-// typedef noude *ndp;
 
-// typedef struct noude {
-//     int data;
-//     ndp next;
-// }noude;
 
 typedef struct noude noude;
 typedef noude *ndp;
@@ -17,6 +11,8 @@ typedef struct noude {
     int data;
     ndp next;  
 } noude;
+
+void append(ndp *head,int dt);
 
 void prepend(ndp *head,int dt){ 
     ndp hp ;       //helper pionter 
@@ -29,30 +25,38 @@ void prepend(ndp *head,int dt){
     
 }
 
-// ndp insrt(ndp head,int dt,int index){ 
-//     ndp hp , scnr = head;       //helper pionter
+void insrt(ndp head,int dt,int index){ 
+    ndp hp , scnr = head;       //helper pionter
     
-//     if (index == 0){
-//         return prepend(head , dt);
+    if (index == 0){
+        prepend(&head , dt);
     
-//     }
-//     else if (index == -1){
-//         return append(head,dt);
-//     }
-    
+    }
 
-//     // }
-
-//     // else{ 
-//     //     for (int cnt = 0; cnt <= index; cnt ++){
-//     //       scnr = (*scnr).next; 
-//     //     }
-//     // }
-//     // hp = malloc(sizeof(noude));
+    else if (index == -1){
+        append(&head,dt);
+    }
     
-// }
-void append(ndp head,int dt){ 
-    ndp scnr = head ,new_noude;       //helper pionter
+      
+    else{ 
+        hp = malloc(sizeof(noude));
+        (*hp).data = dt;
+
+        scnr = head; 
+
+        for (int pos = 0; pos < index - 1; pos ++){
+            scnr = (*scnr).next; 
+        }
+
+        (*hp).next   = (*scnr).next ;
+        (*scnr).next = hp;
+    }
+    
+}
+ 
+
+void append(ndp *head,int dt){ 
+    ndp scnr = *head ,new_noude;       //helper pionter
     
     new_noude = malloc(sizeof(noude));
     (*new_noude).data = dt;
@@ -60,10 +64,8 @@ void append(ndp head,int dt){
 
     while ((*scnr).next != NULL){
         scnr = (*scnr).next;
-        printf("%d\n",scnr);
     }
     
-
     (*scnr).next = new_noude;
 }
 
@@ -80,13 +82,11 @@ void display(ndp head){
 void main(int argc, char const *argv[]){
     ndp hd = NULL; // hd mean head
     prepend(&hd ,1);
-    printf("%p\n",hd);
     prepend(&hd ,2);
-    printf("%p\n",hd);    
     prepend(&hd ,3);
-    printf("%p\n",hd);
-    // append(hd,5);
+    insrt(hd,55,0);
+    prepend(&hd ,777);
+
     display(hd);
-    
     
 }
